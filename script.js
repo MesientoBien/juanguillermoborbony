7020257dd76141b21ccaef7613c5a21bbd5b6561
MesientoBien <118586914+MesientoBien@users.noreply.github.com>
2026-08-01 10:49:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-
-    const menuButton = document.getElementById('menuButton');
+const menuButton = document.getElementById('menuButton');
     const mainNav = document.getElementById('mainNav');
 
     menuButton.addEventListener('click', () => {
-      mainNav.classList.toggle('is-open');
+        mainNav.classList.toggle('is-open');
     });
 
-    // cierra el menú al hacer clic en cualquier link (útil en móvil)
+    // Cierra el menú al hacer clic en un enlace
     mainNav.querySelectorAll('a').forEach(link => {
-      link.addEventListener('click', () => mainNav.classList.remove('is-open'));
+        link.addEventListener('click', () => {
+            mainNav.classList.remove('is-open');
+        });
     });
+
+
+    /* ==========================
+       PERSONAJE PORTADA
+    ========================== */
+
+    const heads = document.querySelectorAll(".character-img");
+    const characterBox = document.querySelector(".character-placeholder");
+
+    let currentHead = 0;
+
+    setInterval(() => {
+
+        heads[currentHead].classList.remove("active");
+
+        currentHead++;
+
+        if (currentHead >= heads.length) {
+            currentHead = 0;
+        }
+
+        heads[currentHead].classList.add("active");
+
+        characterBox.classList.remove("shake");
+
+        void characterBox.offsetWidth;
+
+        characterBox.classList.add("shake");
+
+    }, 500);
